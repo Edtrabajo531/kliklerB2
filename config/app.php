87a90efee82096de12bec779ace81250f5cc7be0
unknown <edtrabajo531@gmail.com>
2022-04-14 09:02:06 -0500

    14-4-2022

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class
+        Intervention\Image\ImageServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
     ],
 
     /*
